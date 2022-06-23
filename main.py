1f52d740d5b18ad7a9db3fb4f09f29ab0d8cedb0
Varg <vargaloid@gmail.com>
2022-06-23 20:10:44 +0300

    Bot commands menu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from sys import exit
 from os import getenv
 from loguru import logger
+from aiogram.types import BotCommand
 from aiogram import Bot, Dispatcher, types
 
 from bot.handlers.bot_commands import register_bot_commands
@@ -12,6 +13,13 @@ from bot.handlers.bot_messages import register_bot_messages
 def register_all_handlers(dp):
     register_bot_commands(dp)
     register_bot_messages(dp)
+
+
+async def set_bot_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Start bot"),
+    ]
+    await bot.set_my_commands(commands)
 
 
 async def main():
@@ -27,7 +35,6 @@ async def main():
                filter=None, colorize=None, backtrace=True, diagnose=True, enqueue=False, catch=True,
                rotation="1 days", compression="zip")
 
-
     bot_token = getenv("BOT_TOKEN")
     if not bot_token:
         logger.error('Error: no Bot token provided')
@@ -37,6 +44,8 @@ async def main():
     dp = Dispatcher(bot)
 
     register_all_handlers(dp)
+
+    await set_bot_commands(bot)
 
     logger.info('Starting bot..')
 
